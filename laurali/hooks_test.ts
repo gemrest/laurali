1ff09dab42263ad5f80ecccaa4a070ba835c741b
Fuwn <contact@fuwn.me>
2022-05-15 06:45:02 +0000

    ci(laurali): add a test so ci won't freak out

---
+++ laurali/hooks_test.ts
@@ -0,0 +1,22 @@
+// This file is part of Laurali <https://github.com/gemrest/laurali>.
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { Hook } from "./hooks.ts";
+import { assertEquals } from "https://deno.land/std@0.139.0/testing/asserts.ts";
+
+Deno.test("Hook", () => assertEquals(Hook.ON_ERROR, 2));
